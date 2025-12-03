85bd803e21eb9c562e35de448332c7568482987e
Marta2852 <ipa23.m.dzerve@vtdt.edu.lv>
2025-12-03 14:20:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Jägermeister</h1>
+    <p>Jägermeister is a German digestif made with 56 herbs and spices. 
+        Developed in 1934 by Wilhelm and Curt Mast, it has an alcohol by volume of 35%.
+         The recipe has not changed since its creation, and the drink continues to be sold in a green glass bottle.
+         </p>
+
+        <img src=https://secretbottleshop.co.uk/cdn/shop/files/dc194924-3610-4d89-acb8-5c59647f6f43_700x700.jpg?v=1748337973 width="300" height="300" >
+</body>
+</html>
